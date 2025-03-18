439c5f22c6480947937324646d82727bedd9a526
hyun <apfhd0257@naver.com>
2025-03-18 12:32:57 +0900

    동시성 문제 - 코드 단위synchornized를 이용한 해결

---
+++ src/main/java/thread/sync/BankAccountV3.java
@@ -0,0 +1,42 @@
+package thread.sync;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV3 implements BankAccount {
+    private int balance;
+
+    public BankAccountV3(int balance) {
+        this.balance = balance;
+    }
+
+    // 동기화된 메서드: 한 번에 하나의 스레드만 실행 가능
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작: " + getClass().getSimpleName());
+        // 잔고가 출금액 보다 적으면, 진행하면 안됨
+        // 안전한 임계 영역 지정
+        // this: 락을 얻는 곳을 지정
+        synchronized (this) {
+            log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
+            if (balance < amount) {
+                log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
+                if (balance < amount) {
+                    return false;
+                }
+            }
+            // 잔고가 출금액 보다 많으면 진행
+            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
+            sleep(1000);
+            balance = balance - amount;
+            log("[출금 완료] 출금액: " + amount + ", 잔액: " + balance);
+        }
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public synchronized int getBalance() {
+        return balance;
+    }
+}
