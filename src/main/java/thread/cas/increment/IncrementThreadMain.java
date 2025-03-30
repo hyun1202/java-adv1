e73d292107fe0b192be6115f085ae2a63fd6f399
hyun <apfhd0257@naver.com>
2025-03-30 22:16:13 +0900

    원자적 연산

---
+++ src/main/java/thread/cas/increment/IncrementThreadMain.java
@@ -0,0 +1,36 @@
+package thread.cas.increment;
+
+import java.util.ArrayList;
+
+import static util.ThreadUtils.sleep;
+
+public class IncrementThreadMain {
+    public static final int THREAD_COUNT = 1000;
+
+    public static void main(String[] args) throws InterruptedException {
+        test(new BasicInteger());
+    }
+
+    private static void test(IncrementInteger incrementInteger) throws InterruptedException {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                sleep(10);  //너무 빨리 실행되므로 다른 스레드와 동시 실행을 위해 잠시 쉰다.
+                incrementInteger.increment();
+            }
+        };
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i=0; i<THREAD_COUNT; i++) {
+            Thread thread = new Thread(runnable);
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        int result = incrementInteger.get();
+        System.out.println(incrementInteger.getClass().getSimpleName() + " result: " + result);
+    }
+}
