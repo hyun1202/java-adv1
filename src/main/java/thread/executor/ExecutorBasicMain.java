ca1f6ab4f98600bcff0dfe17f7b021865439270a
hyun <apfhd0257@naver.com>
2025-03-31 13:23:12 +0900

    Thread pool executor

---
+++ src/main/java/thread/executor/ExecutorBasicMain.java
@@ -0,0 +1,32 @@
+package thread.executor;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import static thread.executor.ExecutorUtils.printState;
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class ExecutorBasicMain {
+    public static void main(String[] args) {
+        // LinkedBlockingQueue를 사용하면 작업을 무한대로 저장할 수 있다(메모리가 허용하는 최대치)
+        ThreadPoolExecutor es = new ThreadPoolExecutor(2, 2, 0, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+        log("== 초기 상태 ==");
+        printState(es);
+        es.execute(new RunnableTask("taskA"));
+        es.execute(new RunnableTask("taskB"));
+        es.execute(new RunnableTask("taskC"));
+        es.execute(new RunnableTask("taskD"));
+        log("== 작업 수행 중 ==");
+        printState(es);
+
+        sleep(3000);
+        log("== 작업 수행 완료 ==");
+        printState(es);
+
+        es.close();
+        log("== shutdown 완료 ==");
+        printState(es);
+    }
+}
