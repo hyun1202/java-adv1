5f74e28a2d99f869e6a85811d2b9aa7038c1c369
hyun <apfhd0257@naver.com>
2025-03-31 18:21:04 +0900

    문제 풀이 - 주문 프로세스 최적화 Excutors와 Future를 이용한 작업 비동기 처리

--- src/main/java/thread/executor/test/OldOrderService.java
+++ src/main/java/thread/executor/test/OldOrderService.java
@@ -1,26 +1,36 @@
 package thread.executor.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
 public class OldOrderService {
-    public void order(String orderNo) {
-        InventoryWork inventoryWork = new InventoryWork(orderNo);
-        ShippingWork shippingWork = new ShippingWork(orderNo);
-        AccountingWork accountingWork = new AccountingWork(orderNo);
+    public void order(String orderNo) throws InterruptedException, ExecutionException {
+        ExecutorService es = Executors.newFixedThreadPool(10);
+        Callable<Boolean> inventoryWork = new InventoryWork(orderNo);
+        Callable<Boolean> shippingWork = new ShippingWork(orderNo);
+        Callable<Boolean> accountingWork = new AccountingWork(orderNo);
+        Future<Boolean> inventoryFuture = es.submit(inventoryWork);
+        Future<Boolean> shippingFuture = es.submit(shippingWork);
+        Future<Boolean> accountFuture = es.submit(accountingWork);
 
-        Boolean inventoryResult = inventoryWork.call();
-        Boolean shippingResult = shippingWork.call();
-        Boolean accountingResult = accountingWork.call();
+        Boolean inventoryResult = inventoryFuture.get();
+        Boolean shippingResult = shippingFuture.get();
+        Boolean accountingResult = accountFuture.get();
 
         if (inventoryResult && shippingResult && accountingResult) {
             log("모든 주문 처리가 성공적으로 완료되었습니다.");
         } else {
             log("일부 작업이 실패했습니다.");
         }
+        es.close();
     }
 
-    static class InventoryWork {
+    static class InventoryWork implements Callable<Boolean> {
         private final String orderNo;
 
         public InventoryWork(String orderNo) {
@@ -34,7 +44,7 @@ public class OldOrderService {
         }
     }
 
-    static class ShippingWork {
+    static class ShippingWork implements Callable<Boolean> {
         private final String orderNo;
 
         public ShippingWork(String orderNo) {
@@ -48,7 +58,7 @@ public class OldOrderService {
         }
     }
 
-    static class AccountingWork {
+    static class AccountingWork implements Callable<Boolean> {
         private final String orderNo;
 
         public AccountingWork(String orderNo) {
